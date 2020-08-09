424ffc8f11d867d13003a9672c88da56213159fb
cibilsmash <54404426+cibilsmash@users.noreply.github.com>
2020-08-09 17:40:06 +0530

    Add files via upload

--- base.py
+++ base.py
@@ -19,97 +19,111 @@ Session = sessionmaker(bind=engine)
 
 
 
-class Employees(Base):
+class Employee(Base):
 
     __tablename__ = 'employees'
 
     emp_no = Column(Integer(), primary_key=True)
 
-    birth_date = Column(DateTime(), default = datetime.now)
+    birth_date = Column(DateTime(), default = datetime.date)
 
     first_name = Column(String(14), nullable=False)
 
     last_name = Column(String(16), nullable=False)
 
+    gender = Column(Enum('M','F'))
+
     hire_date = Column(DateTime(), default=datetime.now)
 
-    dep_emp_map = relationship("Dep_emp", backref='employee')
+    dep_emp_map = relationship("Dep_Emp", backref='employee')
 
-    dep_man_map = relationship("Dep_man", backref='employee')
+    dep_man_map = relationship("Dep_Man", backref='employee')
 
-    salary = relationship("Salaries", backref='employee')
+    salary = relationship("Salary", backref='employee')
 
-    title = relationship("Titles", backref= 'employee')
-
-    
+    title = relationship("Title", backref= 'employee')
 
 
 
-    
-
-
-class Departments(Base):
+class Department(Base):
 
     __tablename__ = 'departments'
-
-   
 
     dep_no = Column(Integer(), primary_key=True)
 
     dep_name = Column(String(40))
 
-    dep_emp_map = relationship("Dep_emp", backref='department')
+    dep_emp_map = relationship("Dep_Emp", backref='department')
 
-    dep_man_map = relationship("Dep_man", backref='department')
+    dep_man_map = relationship("Dep_Man", backref='department')
 
+    
 
-class Dep_emp(Base):
+class Dep_Emp(Base):
 
     __tablename__ = 'dep_emp'
 
     id = Column(Integer(), primary_key=True)
 
-    emp_no = Column(Integer(), ForeignKey(Employees.emp_no))
+    emp_no = Column(Integer(), ForeignKey(Employee.emp_no))
 
-    dep_no = Column(Integer(), ForeignKey(Departments.dep_no))
+    dep_no = Column(Integer(), ForeignKey(Department.dep_no))
 
+    from_date = Column(DateTime(), default=datetime.now)
 
-class Dep_man(Base):
+    to_date = Column(DateTime(), default=datetime.now)
+
+    
+class Dep_Man(Base):
 
     __tablename__ = 'dep_man'
 
     id = Column(Integer(), primary_key=True)
 
-    emp_no = Column(Integer(), ForeignKey(Employees.emp_no))
+    dep_no = Column(Integer(), ForeignKey(Department.dep_no))
 
-    dep_no = Column(Integer(), ForeignKey(Departments.dep_no))
+    emp_no = Column(Integer(), ForeignKey(Employee.emp_no))
+
+    from_date = Column(DateTime(), default=datetime.now)
+
+    to_date = Column(DateTime(), default=datetime.now)
 
 
-class Salaries(Base):
+class Salary(Base):
 
     __tablename__ = 'salaries'
 
     id = Column(Integer(), primary_key=True)
 
-    emp_no = Column(Integer(), ForeignKey(Employees.emp_no))
+    emp_no = Column(Integer(), ForeignKey(Employee.emp_no))
 
     salary = Column(Integer())
 
+    from_date = Column(DateTime(), default=datetime.now)
 
-class Titles(Base):
+    to_date = Column(DateTime(), default=datetime.now)
+
+
+class Title(Base):
 
     __tablename__ = 'titles'
 
     id = Column(Integer(), primary_key=True)
 
-    emp_no = Column(Integer(), ForeignKey(Employees.emp_no))
+    emp_no = Column(Integer(), ForeignKey(Employee.emp_no))
 
     title = Column(String(40), nullable=False)
 
+    from_date = Column(DateTime(), default=datetime.now)
+
+    to_date = Column(DateTime(), default=datetime.now)
 
 
 
-Base.metadata.drop_all(engine)
+
+Base.metadata.create_all(engine)
+
+
 
 
 
